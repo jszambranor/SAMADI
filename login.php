e59a5f4da523e4c8689ef9d62066737c8f5a5f3f
jszambranor <50810021+jszambranor@users.noreply.github.com>
2019-09-08 01:30:45 -0500

    ESTRUCTURA DEL LOGIN

---
+++ login.php
@@ -0,0 +1,80 @@
+<?php
+$user = $_POST['user'];
+$password = $_POST['password'];
+require_once './conexion/ClassConexion.php';
+require_once './model/ClassModelConsultas.php';
+require_once './estruct/header.php';
+/*if (!isset($_SESSION['user'])) {
+  header('location: ../../');
+}else{
+  $_SESSION['user'];
+  $_SESSION['type_user'];
+  $sidenav = new Estruct();
+  $get_Sidenav = $sidenav->get_SideNavAdmin($_SESSION['user']);
+}*/
+$cedula = '0992181293';
+$sidenav = new Estruct();
+$get_Sidenav = $sidenav->get_SideNavAdmin($cedula);
+ ?>
+
+<!DOCTYPE html>
+<html lang="es-ES" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="./css/admin.css">
+    <title>Inicio-ADMINISTRADOR</title>
+  </head>
+  <body>
+    <header>
+      <?php echo $get_Sidenav;  ?>
+    </header>
+
+    <main>
+      <center>
+      <div class="section container">
+          <div id="row1" class="row">
+            <div class="header">
+              <div class="tittle-login">
+                <span>INICIO DE SESION</span>
+              </div>
+            </div>
+              <form class="col s12" method="POST" action="./login.php">
+                  <div class="row card-panel">
+                      <div class="input-field col s12">
+                          <input required="true"  id="user" name="user" type="text" class="validate" onkeyup="Mayus(this);" onkeypress="return soloNumeros(event)">
+                          <label id="select-input" for="user">USUARIO * </label>
+                      </div>
+                      <div class="input-field col s12">
+                          <input required="true" id="password" name="password" type="text" class="validate" onkeyup="Mayus(this);" onkeypress="return soloLetras(event)">
+                          <label id="select-input" for="password">CONTRASEÑA *</label>
+                      </div>
+
+                    <center>
+                      <div class="input-field col s12">
+                          <button class="btn waves-effect waves-light" type="submit" name="action" id="btn">INICIAR SESION
+                              <i class="material-icons left">send</i>
+                          </button>
+                      </div>
+                    </center>
+                  </div>
+              </form>
+          </div>
+        </div>
+      </center>
+    </main>
+
+
+    <style media="screen">
+    #inicio{
+      background-color: #D6EAF8;
+      border-radius: 0px 50px 50px 0px;
+      width: 90%;
+    }
+    </style>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript" src="./js/init.js"></script>
+  </body>
+</html>
